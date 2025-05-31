ac0ed5b97c1febe4c8394190eba8d05071afb3f4
viceda-s <vicente.coelho95@gmail.com>
2025-05-31 11:47:07 +0200

    C-Piscine-Reloaded

---
+++ C-Piscine-Reloaded/ex20/ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: viceda-s <viceda-s@student.42luxembourg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/31 11:39:53 by viceda-s          #+#    #+#             */
+/*   Updated: 2025/05/31 11:42:24 by viceda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	if (!src)
+		return (0);
+	while (src[i])
+		i++;
+	dest = (char *)malloc(sizeof(char) * (i + 1));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
